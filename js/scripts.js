0de67e1a1df92c6540d41968131d0c809dbb82ef
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-11 19:47:11 -0600

    add switchLetters function, commented out test code in js

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,23 @@ $(document).ready(function() {
   const sentence = prompt("Enter sentence");
   const firstLetter = sentence.charAt(0);
   const lastLetter = sentence.charAt(sentence.length-1);
-  // function capLetters(firstLetter, lastLetter){
-  //   return first.toUpperCase + lastLetter.toUpperCase;
-  //   firstletter.toUpperCase
-  // }
-  const slicedString = sentence.slice(1,sentence.length-1)
+    
+  //function capLetters() {
+    //return firstLetter.toUpperCase + lastLetter.toUpperCase;
+  //}
+
+  //alert(capLetters(sentence));
+
+
+  function switchLetters() {
+      return lastLetter + firstLetter;
+    }
+  
+  alert(switchLetters(sentence));
+  
+  //const capString = capLetters();
+
+  const slicedString = sentence.slice(1,sentence.length-1);
   alert(lastLetter + slicedString + firstLetter);
 });
 
