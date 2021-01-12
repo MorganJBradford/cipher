0350ef3d12566f36e6bdd748df6b406a82cba360
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-11 19:04:38 -0600

    declare const: firstLetter, lastLetter, slicedString; .sliced const sentence
    Co-authored-by: Logan Roth <diamondintheroth@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
   const sentence = prompt("Enter sentence");
-  alert(sentence);
+  const firstLetter = sentence.charAt(0);
+  const lastLetter = sentence.charAt(sentence.length-1);
+  // function capLetters(firstLetter, lastLetter){
+  //   return first.toUpperCase + lastLetter.toUpperCase;
+  //   firstletter.toUpperCase
+  // }
+  const slicedString = sentence.slice(1,sentence.length-1)
+  alert(lastLetter + slicedString + firstLetter);
 });
 
