57f79bb56a24d610768caf42aa589ed0268e7493
Morgan Bradford <morganjbradford95@gmail.com>
2021-01-11 21:20:22 -0600

    add capSwitch function(not functional at this time)

--- js/scripts.js
+++ js/scripts.js
@@ -3,22 +3,19 @@ $(document).ready(function() {
   const firstLetter = sentence.charAt(0);
   const lastLetter = sentence.charAt(sentence.length-1);
     
-  //function capLetters() {
-    //return firstLetter.toUpperCase + lastLetter.toUpperCase;
-  //}
-
-  //alert(capLetters(sentence));
-
+  function capLetters() {
+    return firstLetter.toUpperCase() + lastLetter.toUpperCase();
+  }
 
   function switchLetters() {
-      return lastLetter + firstLetter;
-    }
+    return lastLetter + firstLetter;
+  }
   
+  function capSwitch() {
+    return lastLetter.toUpperCase() + firstLetter.toUpperCase();
+  }
+
+  alert(capLetters(sentence));
   alert(switchLetters(sentence));
-  
-  //const capString = capLetters();
-
-  const slicedString = sentence.slice(1,sentence.length-1);
-  alert(lastLetter + slicedString + firstLetter);
+  alert(capSwitch(sentence));
 });
-
